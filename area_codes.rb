9d197c9055d3e67c9e030ce818929ed117e17de0
Rob Farmer <rob.farmer@chargify.com>
2019-11-23 20:38:25 -0500

    area codes

---
+++ area_codes.rb
@@ -0,0 +1,21 @@
+dial_book = {
+  "atlanta" => "404",
+  "ocala" => "123",
+}.freeze
+
+def get_city_names(somehash)
+  somehash.each_key { |key| puts key }
+end
+
+def get_area_code(somehash, key)
+  somehash[key]
+end
+
+loop do
+  print "Do you want to look up an area code by city name? [Y/N] "
+  break if gets.chomp.downcase != "y"
+  get_city_names(dial_book)
+  print ":> "
+  user_request = gets.chomp
+  puts get_area_code(dial_book, user_request) || "<not found>"
+end
